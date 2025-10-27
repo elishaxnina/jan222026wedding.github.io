a274a44e30c562874c2a3f38cb99b03ec531103a
shielasan96 <shielacabahug96@gmail.com>
2025-10-27 20:41:42 +0800

    transfer js to index.html

--- script.js
+++ script.js
@@ -466,19 +466,6 @@ document.addEventListener('DOMContentLoaded', function() {
                 element.style.transform = `translateY(${scrolled * speed}px) rotate(${scrolled * 0.1}deg)`;
             });
         });
-     // Smooth scroll
-    document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-        anchor.addEventListener('click', function (e) {
-            e.preventDefault();
-            const target = document.querySelector(this.getAttribute('href'));
-            if (target) {
-                target.scrollIntoView({
-                    behavior: 'smooth',
-                    block: 'start'
-                });
-            }
-        });
-    });
 });
     
 
